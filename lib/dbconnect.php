7ca73ba7348fc41c208482be22674ef1f0e8a304
VareltziChristina <it123832@it.teithe.gr>
2021-01-02 17:59:51 +0200

    01/02/2020

--- lib/dbconnect.php
+++ lib/dbconnect.php
@@ -1,6 +1,10 @@
 <?php
 $host='localhost';
+<<<<<<< HEAD
 $db = 'mytavli';
+=======
+$db = 'adise20_chess1';
+>>>>>>> 6eb3309babc096ae59f1afd99ce85f8ce043ce48
 require_once "config_local.php";
 
 $user=$DB_USER;
@@ -8,7 +12,11 @@ $pass=$DB_PASS;
 
 
 if(gethostname()=='users.iee.ihu.gr') {
+<<<<<<< HEAD
 	$mysqli = new mysqli($host, $user, $pass, $db,null,'/home/student/it/2012/it123832/mysql/run/mysql.sock');
+=======
+	$mysqli = new mysqli($host, $user, $pass, $db,null,'/home/staff/asidirop/mysql/run/mysql.sock');
+>>>>>>> 6eb3309babc096ae59f1afd99ce85f8ce043ce48
 } else {
         $mysqli = new mysqli($host, $user, $pass, $db);
 }
